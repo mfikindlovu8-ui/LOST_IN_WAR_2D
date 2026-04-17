a8ecc0edc453fa6e30d2f29ae65b569937514f21
G4-MySecret-P4ssword <mokoenadisebo440@gmail.com>
2026-04-17 15:17:24 +0200

    Player and Enemy Scripts
    Player movement script, enemy attack script and player attack script.

---
+++ Assets/Player Movement.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    float moveX;
+    float moveY;
+    float movementspeed= 5f;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+
+         
+        
+     
+    }
+
+    void Update()
+    {
+        moveX = Input.GetAxis ("Horizontal");
+        moveY = Input.GetAxis ("Vertical");
+
+        Vector3 movement= new Vector3(moveX, moveY,0);
+
+        transform.Translate(movement*movementspeed*Time.deltaTime);
+        
+    }
+}
+        
+    
+
+
+    
+        
