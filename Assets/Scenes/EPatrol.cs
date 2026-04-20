7a11606d483f77bc954fc12a0bf0bfb300eaef57
mfikindlovu8-ui <mfiki.ndlovu8@gmail.com>
2026-04-20 09:22:47 +0200

    Enemy Patrol

--- Assets/Scenes/EPatrol.cs
+++ Assets/Scenes/EPatrol.cs
@@ -7,6 +7,7 @@ public class EPatrol : MonoBehaviour
     private Rigidbody2D rb;
     private Transform currentPoint;
     public float speed;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
